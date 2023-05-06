a68c6d6d30771a008925eada228d341fa0936afc
Gong <Gong@qq.com>
2023-05-06 16:21:14 +0800

    second

--- src/main/java/com/study/main/Testmain.java
+++ src/main/java/com/study/main/Testmain.java
@@ -4,8 +4,6 @@ public class Testmain {
     public static void main(String[] args) {
         System.out.println("龚井斌啊");
         System.out.println("龚井斌啊");
-        System.out.println("龚井斌啊");
-        System.out.println("龚井斌啊");
-        System.out.println("龚井斌啊");
+        System.out.println("hello git2");
     }
 }
