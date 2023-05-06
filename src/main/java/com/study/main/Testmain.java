be72de0b7810e4b7262694818e93c778874b5607
Gong <Gong@qq.com>
2023-05-06 16:39:44 +0800

    hot-fix merge

--- src/main/java/com/study/main/Testmain.java
+++ src/main/java/com/study/main/Testmain.java
@@ -4,8 +4,6 @@ public class Testmain {
     public static void main(String[] args) {
         System.out.println("龚井斌啊");
         System.out.println("龚井斌啊");
-        System.out.println("hello git2");
-        System.out.println("hello git3");
-        System.out.println("hello git4");
+        System.out.println("hot-fix code");
     }
 }
