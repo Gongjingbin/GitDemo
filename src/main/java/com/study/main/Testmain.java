55d59eb77c5fd1240e905528b3ea875073608e09
Gong <Gong@qq.com>
2023-05-06 16:17:31 +0800

    first commit

---
+++ src/main/java/com/study/main/Testmain.java
@@ -0,0 +1,11 @@
+package com.study.main;
+
+public class Testmain {
+    public static void main(String[] args) {
+        System.out.println("龚井斌啊");
+        System.out.println("龚井斌啊");
+        System.out.println("龚井斌啊");
+        System.out.println("龚井斌啊");
+        System.out.println("龚井斌啊");
+    }
+}
